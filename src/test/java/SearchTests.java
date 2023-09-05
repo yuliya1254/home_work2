b0fe07c1087d47958b8a2ff4cf1831f94cca8b0c
uliamensikova <yu.menschikowa2015@yandex.ru>
2023-09-05 21:38:09 +0300

    done!

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -10,5 +10,6 @@ public class SearchTests {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+
     }
 }
